8c640da5a0a920c6f7ec6744231fbe2de9785f37
andypower2016 <73726447+andypower2016@users.noreply.github.com>
2021-11-22 16:15:04 +0800

    Add files via upload

--- atm/atm.cpp
+++ atm/atm.cpp
@@ -1,71 +1,107 @@
 #include "systemc.h"
 
+const int size = 32;
+const int atm_op_time = 5; // atm operation time (ns)
+
 SC_MODULE(ATM)
 {
 
-  void Person()
+  void DummyManWithDraw()
   {
     while(true)
     {
       wait();
-      withdraw = m_withdraw.read();
-      e_withDraw.notify(SC_ZERO_TIME);
-
-      cout <<"@" << sc_time_stamp() << " withdrawing money, withdraw ammount=" << withdraw << std::endl;
-      cout <<"@" << sc_time_stamp() << " waiting for atm to complete ..." << std::endl;
-      wait(e_withDrawComplete);
+      if(!bDeposit)
+      {
+        withdraw = rand() % 1000;
+        m_Withdraw.write(withdraw);
+        eWithDraw.notify(SC_ZERO_TIME);
+        cout <<"@" << sc_time_stamp() << " DummyManWithDraw() withdrawing money, withdraw ammount=" << withdraw << std::endl;
+        cout <<"@" << sc_time_stamp() << " DummyManWithDraw() waiting for atm to complete ..." << std::endl;
+        wait(eWithDrawComplete);
+      } 
     }
+  }
 
+  void DummyManDeposit()
+  {
+    while(true)
+    {
+      wait(eDeposit);
+      int deposit = rand() % 2000;
+      total += deposit;
+      cout <<"@" << sc_time_stamp() << " DummyManDeposit() depositing money, deposit ammount=" << deposit << std::endl;
+      wait(atm_op_time, SC_NS);
+      eDepositComplete.notify(SC_ZERO_TIME);
+    }
   }
   
   void AtmHandler()
   { 
+    m_AccountTotal.write(total);
     while(true)
     {
-      wait();
+      wait(eWithDraw);
+
+      // deposit if total < withdraw
       if(total < withdraw)
       {
-        total = withdraw * 2;
+        bDeposit = true;
+        eDeposit.notify(SC_ZERO_TIME);
+        wait(eDepositComplete);
+        bDeposit = false;
+        
+        char msg[100];
+        sprintf(msg, "AtmHandler() Deposit Complete ! Account Total = %d", total);
+        cout <<"@" << sc_time_stamp() << msg << std::endl;
       }
+      
+      // continue withdraw after deposit
       total -= withdraw;
-      wait(5, SC_NS);
-      cout <<"@" << sc_time_stamp() << " atm complete withdraw" << std::endl;
-      m_total.write(total);
-      e_withDrawComplete.notify(SC_ZERO_TIME);
+      wait(atm_op_time, SC_NS);
+      cout <<"@" << sc_time_stamp() << "AtmHandler() atm complete withdraw" << std::endl;
+      m_AccountTotal.write(total);
+      eWithDrawComplete.notify(SC_ZERO_TIME);
     }
   }
 
   void DisplayTotal()
   {
      cout <<"@" << sc_time_stamp() <<
-        " :: Total Value "<< m_total.read() << endl;
+        " DisplayTotal() Account total = "<< m_AccountTotal.read() << endl;
   }
 
   SC_CTOR(ATM) 
   {    
     SC_THREAD(AtmHandler);
-    sensitive << e_withDraw;
 
-    SC_THREAD(Person);
+    SC_THREAD(DummyManWithDraw);
     sensitive << m_clock.pos();
-    
-    SC_METHOD(DisplayTotal);
-    sensitive << m_total;
-    dont_initialize();
 
-    total = 31;
+    SC_THREAD(DummyManDeposit);
+    
+    total = 1000;
+    bDeposit = false;
+    SC_METHOD(DisplayTotal);
+    sensitive << m_AccountTotal;
   }
 
   // local vars
-  sc_uint<4> withdraw;
-  sc_uint<5> total;
+  int withdraw;
+  int total;
+  bool bDeposit;
 
   // ATM 
-  sc_event e_withDraw;
-  sc_event e_withDrawComplete;
+  sc_event eDeposit;
+  sc_event eDepositComplete;
+
+  sc_event eWithDraw;
+  sc_event eWithDrawComplete;
+
   sc_in<bool> m_clock;
-  sc_in<sc_uint<4>> m_withdraw;
-  sc_out<sc_uint<5>> m_total;
+  sc_out<sc_uint<size>> m_Deposit;
+  sc_out<sc_uint<size>>  m_Withdraw;
+  sc_out<sc_uint<size>> m_AccountTotal;
   
 };
 
@@ -75,30 +111,23 @@ int sc_main(int argc, char* argv[])
   ATM atm("ATM");
 
   sc_clock clock("clock", 1, SC_NS, 0.5, 0, SC_NS, true);
-  sc_signal<sc_uint<4>> withdraw;
-  sc_signal<sc_uint<5>> total;
+  sc_signal<sc_uint<size>> withdraw;
+  sc_signal<sc_uint<size>> deposit;
+  sc_signal<sc_uint<size>> account_total;
 
-  atm.m_withdraw(withdraw);
+  atm.m_Withdraw(withdraw);
   atm.m_clock(clock);
-  atm.m_total(total);
+  atm.m_AccountTotal(account_total);
+  atm.m_Deposit(deposit);
 
   // trace file
   sc_trace_file *wf = sc_create_vcd_trace_file("./atm_trace");
   sc_trace(wf, clock, "clock");
   sc_trace(wf, withdraw, "withdraw");
-  sc_trace(wf, total, "total");
+  sc_trace(wf, account_total, "account_total");
+  sc_trace(wf, deposit, "deposit");
 
-  withdraw = 10;
-  for(int i = 0 ; i < 10 ; ++i)
-  {
-    sc_start(1, SC_NS);
-  }
-  withdraw = 1;
-  for(int i = 0 ; i < 10 ; ++i)
-  {
-    sc_start(1, SC_NS);
-  }
-
+  sc_start(100, SC_NS);
   sc_close_vcd_trace_file(wf);
   sc_stop();
 
